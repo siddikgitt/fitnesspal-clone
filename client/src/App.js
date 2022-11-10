d8b8648fac7f7f5b17fdfe360e60f73f681af9be
unknown <shridharhakke1998@gmail.com>
2022-11-10 18:05:00 +0530

    execise page and Food Database has completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-
-import './App.css';
-import CreateNewMyExercise from './components/ExerciseComp/CreateNewMyExercise';
-import AddToDairy from './pages/ExercisePage/AddToDairy';
-import Database_calBurn from './pages/ExercisePage/Database_calBurn';
-import ExerciseMain from './pages/ExercisePage/ExerciseMain';
-import MyExercisePage from './pages/ExercisePage/MyExercisePage';
-
+import "./App.css";
+import CreateNewMyExercise from "./components/ExerciseComp/CreateNewMyExercise";
+import FoodDatabase from "./components/ExerciseComp/FoodDatabase";
+import My_Food from "./components/ExerciseComp/My_Food";
+import AddToDairy from "./pages/ExercisePage/AddToDairy";
+import Database_calBurn from "./pages/ExercisePage/Database_calBurn";
+import ExerciseMain from "./pages/ExercisePage/ExerciseMain";
+import MyExercisePage from "./pages/ExercisePage/MyExercisePage";
 
 function App() {
   return (
     <div>
- 
-    
-    <Database_calBurn/>
-    
-     <MyExercisePage/>
-     <CreateNewMyExercise/>
+      <Database_calBurn />
+
+      <MyExercisePage />
+      <FoodDatabase />
+      <My_Food />
+      <CreateNewMyExercise />
     </div>
   );
 }
