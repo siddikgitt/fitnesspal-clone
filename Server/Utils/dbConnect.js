f51d1a069e5f7f8c9470c942acd4a1b6dd6697ca
siddikgitt <siddikkhanpathan4@gmail.com>
2022-11-13 16:24:56 +0530

    mongodb atlas

--- Server/Utils/dbConnect.js
+++ Server/Utils/dbConnect.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const mongoose=require("mongoose");
 
 const dbConnect=()=>{
-    mongoose.connect(process.env.DATABASE,()=>{
+    mongoose.connect(process.env.DATABASE,(e)=>{
+        console.log(e);
         console.log("Database is connected.");
     })
 }
